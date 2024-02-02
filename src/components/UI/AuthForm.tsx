23c6c192e853de96d8b5bfc80aa04e9fb122ac59
hyunyeee <5004ohy@naver.com>
2024-02-03 08:31:21 +0900

    feat: Input 상태에 따른 UI 디자인 - #1
    - focus
    - 유효성 검사 통과 여부

--- src/components/UI/AuthForm.tsx
+++ src/components/UI/AuthForm.tsx
@@ -3,8 +3,6 @@ import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { schema } from '../../validation/schema';
 import AuthInput from './AuthInput';
-import { ReactComponent as ID_icon } from '../../assets/id_icon.svg';
-import { ReactComponent as PWD_icon } from '../../assets/pwd_icon.svg';
 
 export type FormValues = {
   id: string;
@@ -31,18 +29,14 @@ const AuthForm: React.FC = () => {
           placeholder="아이디"
           register={register}
           errorMsg={errors?.id?.message ?? ''}
-        >
-          <ID_ICON />
-        </AuthInput>
+        />
         <AuthInput
           type="password"
           name="password"
           placeholder="비밀번호"
           register={register}
           errorMsg={errors?.password?.message ?? ''}
-        >
-          <PWD_ICON />
-        </AuthInput>
+        />
       </InputContainer>
       <Button type="submit">로그인</Button>
     </Form>
@@ -67,11 +61,5 @@ const Button = styled.button`
   color: white;
   ${({ theme }) => theme.typographies.BIG_TXT};
   background-color: ${({ theme }) => theme.colors.BLUE_2};
-`;
-const ID_ICON = styled(ID_icon)`
-  width: 24px;
-`;
-const PWD_ICON = styled(PWD_icon)`
-  width: 24px;
 `;
 export default AuthForm;
--- src/components/UI/AuthInput.tsx
+++ src/components/UI/AuthInput.tsx
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
+import { useState } from 'react';
 import { UseFormRegister } from 'react-hook-form';
 import { FormValues } from './AuthForm';
+import { ReactComponent as ID_icon } from '../../assets/id_icon.svg';
+import { ReactComponent as PWD_icon } from '../../assets/pwd_icon.svg';
 
 type InputType = {
-  children: React.ReactNode;
   type: string;
   name: 'id' | 'password';
   placeholder: string;
@@ -11,31 +13,53 @@ type InputType = {
   errorMsg?: string;
 };
 
-const AuthInput = ({
-  children,
+type InputBoxProps = {
+  isFocus: boolean;
+  isError: boolean;
+};
+
+const AuthInput: React.FC<InputType> = ({
   type,
   name,
   placeholder,
   register,
   errorMsg,
 }: InputType) => {
+  const [isFocus, setIsFocus] = useState<boolean>(false);
+
   return (
     <div>
-      <InputBox>
-        {children}
-        <Input type={type} placeholder={placeholder} {...register(name)} />
+      <InputBox isFocus={isFocus} isError={errorMsg !== ''}>
+        {name === 'id' ? (
+          <ID_ICON isFocus={isFocus} isError={errorMsg !== ''} />
+        ) : (
+          <PWD_ICON isFocus={isFocus} isError={errorMsg !== ''} />
+        )}
+        <Input
+          type={type}
+          placeholder={placeholder}
+          {...register(name)}
+          onFocus={() => setIsFocus(true)}
+          onBlur={() => setIsFocus(false)}
+        />
       </InputBox>
       {errorMsg && <HelperText>{errorMsg}</HelperText>}
     </div>
   );
 };
 
-const InputBox = styled.div`
+const InputBox = styled.div<InputBoxProps>`
   padding: 18px;
   display: flex;
   align-items: center;
   gap: 16px;
-  border: 1px solid ${({ theme }) => theme.colors.LIGHT_GRAY};
+  border: 1px solid
+    ${({ theme, isFocus, isError }) =>
+      isError
+        ? theme.colors.RED
+        : isFocus
+          ? theme.colors.BLUE_2
+          : theme.colors.LIGHT_GRAY};
   border-radius: 6px;
 `;
 const Input = styled.input`
@@ -46,5 +70,23 @@ const HelperText = styled.p`
   margin: 4px;
   color: ${({ theme }) => theme.colors.RED};
   ${({ theme }) => theme.typographies.SMALL_TXT};
+`;
+const ID_ICON = styled(ID_icon)<InputBoxProps>`
+  width: 24px;
+  fill: ${({ theme, isFocus, isError }) =>
+    isError
+      ? theme.colors.RED
+      : isFocus
+        ? theme.colors.BLUE_2
+        : theme.colors.LIGHT_GRAY};
+`;
+const PWD_ICON = styled(PWD_icon)<InputBoxProps>`
+  width: 24px;
+  fill: ${({ theme, isFocus, isError }) =>
+    isError
+      ? theme.colors.RED
+      : isFocus
+        ? theme.colors.BLUE_2
+        : theme.colors.LIGHT_GRAY};
 `;
 export default AuthInput;
