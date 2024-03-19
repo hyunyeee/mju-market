693c40f9f77a72e1a496f7cd0e5e2ef81caa8339
hyunyeee <5004ohy@naver.com>
2024-03-19 14:30:58 +0900

    refactor: App.js 구조 변경 - #8

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './styles/GlobalStyle';
+import { theme } from './styles/theme';
+import { GlobalFont } from './styles/GlobalFont';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <GlobalFont />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
